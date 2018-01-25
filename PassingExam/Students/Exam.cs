724f4a730ddc9c8a77382f2712233bec1fe6085c
ArjunaLib <arjunaliberty@gmail.com>
2018-01-25 14:08:11 +0300

    HomeWork

--- PassingExam/Students/Exam.cs
+++ PassingExam/Students/Exam.cs
@@ -1,37 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PassingExamLibrary
+﻿namespace PassingExamLibrary
 {
     public class Exam
     {
         /// <summary>
-        /// Идентификатор экзамена
-        /// </summary>
-        public int id { get; private set; }
-        /// <summary>
         /// Назваие экзамена
         /// </summary>
-        public string name { get; private set; }
+        public string nameExam { get; private set; }
         /// <summary>
         /// Результат сдачи экзамена
         /// </summary>
         public int result { get; set; }
 
-        public Exam(int id, string name)
+        public Exam(string nameExam)
         {
-            this.id = 0;
-            this.name = "";
+            this.nameExam = nameExam;
             this.result = 0;
-        }
-        public Exam(int id, string name, int result)
-        {
-            this.id = id;
-            this.name = name;
-            this.result = result;
         }
     }
 }
